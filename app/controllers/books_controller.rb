81ef420f8a5d174712d73d9676fad7cb42f496b2
Coba0405 <138278415+Coba0405@users.noreply.github.com>
2023-08-05 05:42:56 +0000

    8/５

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
@@ -12,7 +13,6 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @user = User
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
@@ -34,15 +34,15 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
 end
